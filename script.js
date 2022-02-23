8c9946abbe0cb133088d4fe6eb38cc75862ff7ee
Imaad Uddin <imaaduddin27@gmail.com>
2022-02-22 18:56:45 -0600

    getting errors atm

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const number = document.querySelector("num");
-const testButton = document.querySelector("#counter");
+const upButton = document.querySelector("up");
+const downButton = document.querySelector("down");
+// const testButton = document.querySelector("#counter");
 
-let counter = 0;
+let newNumber = number.innerHTML;
+let count = 0;
 
-testButton.addEventListener("click", function() {
-    counter++;
-    console.log(counter);
-})
+const goUp = () => {
+    count = count + 1
+    newNumber = count;
+    console.log(count);
+}
+
+const goDown = () => {
+    count = count - 1;
+    newNumber = count;
+    console.log(count);
+}
+
+upButton.addEventListener("click", goUp);
+downButton.addEventListener("click", goDown);
+
+
+
+// testButton.addEventListener("click", function() {
+//     counter++;
+//     console.log(counter);
+// });
